bf48b9d39320679fc122188772e9362eac93e46e
Gora Khargosh <gora.khargosh@gmail.com>
2011-08-01 11:55:31 +0530

    Backup.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- config-auto-complete.el
+++ config-auto-complete.el
@@ -10,5 +10,11 @@
 (setq ac-dwim 1)
 ;;(setq ac-ignore-case 1)
 
+;; (defvar autopair-modes '(go-mode))
+;; (defun turn-on-autopair-mode () (autopair-mode 1))
+;; (dolist (mode autopair-modes)
+;;   (add-hook (intern (concat (symbol-name mode) "-hook")) 'turn-on-autopair-mode))
+
+
 
 (provide 'config-auto-complete)
